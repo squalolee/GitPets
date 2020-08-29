351a5891ab86e724825e2b09d6248d5a068d88c4
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-08-29 11:09:42 -0400

    adding nav and footer

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <a href="https://github.com/squalolee/GitPets" target="blank">
+          GitHub Repo
+        </a>
+      </footer>
+    );
+  }
+  
+  export default Footer;

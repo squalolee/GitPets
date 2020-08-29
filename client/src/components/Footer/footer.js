81d4a0319e3f212993229642c1ccc3bb1a1e764c
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-08-29 16:48:45 -0400

    updating footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Footer() {
     return (
-      <footer className="footer">
+      <div className="footer">
         <a href="https://github.com/squalolee/GitPets" target="blank">
           GitHub Repo
         </a>
-      </footer>
+      </div>
     );
   }
   

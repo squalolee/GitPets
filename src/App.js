1b6048ac89d6c69629100354f4ed72796d0dab3d
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-08-29 11:31:39 -0400

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <h1>GitPets</h1>
-    </div>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/search" component={Search} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

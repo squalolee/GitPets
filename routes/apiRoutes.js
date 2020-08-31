bfb52afc668ef8eab54ce161916896c126357ee4
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-08-31 19:53:05 -0400

    adding api info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,32 @@
-module.exports = function(app) {
-    app.get("/api/", function(req, res) {
+const key = process.env.key;
+const secret = process.env.secret;
+let token = '';
+const { exec } = require('child_process');
+
+module.exports = function (app) {
+
+    app.get("/api/", function (req, res) {
         console.log("hi");
+       
+        exec(`curl -d "grant_type=client_credentials&client_id=${key}&client_secret=${secret}" https://api.petfinder.com/v2/oauth2/token`, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            token = JSON.parse(stdout).access_token;
+            console.log(token);
+            exec(`curl -H "Authorization: Bearer ${token}" GET https://api.petfinder.com/v2/animals?type=dog`, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.send(JSON.parse(stdout));
+        });
+        });
+
+        
     });
 
 }
+
+

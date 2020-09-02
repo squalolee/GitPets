568fe74d98de901883eeb2b8f83aa09c87ebff94
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-09-02 14:38:21 -0400

    updating table

--- client/build/precache-manifest.a493651c82fd2ec8061704fbc5359eb5.js
+++ client/build/precache-manifest.a493651c82fd2ec8061704fbc5359eb5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d95c9951479338b7c5878e908fd5286b",
+    "revision": "3c2ec76cbd0156ed27379aea84dbdaab",
     "url": "/index.html"
   },
   {
-    "revision": "080c13b76c14d931d4f8",
+    "revision": "f145f23a3393cc6ab462",
     "url": "/static/css/main.4ca22134.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.8e5b0b5f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "080c13b76c14d931d4f8",
-    "url": "/static/js/main.edb49318.chunk.js"
+    "revision": "f145f23a3393cc6ab462",
+    "url": "/static/js/main.77e19657.chunk.js"
   },
   {
     "revision": "c20b7e9229712c76d7a0",

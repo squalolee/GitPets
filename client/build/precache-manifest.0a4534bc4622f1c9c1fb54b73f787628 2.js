21174bdc5346eaae08ddec00834688c3c1aae868
Nicole Apsche <napsche@gmail.com>
2020-09-19 16:43:34 -0400

    adding express session

---
+++ client/build/precache-manifest.0a4534bc4622f1c9c1fb54b73f787628 2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d37edeea8503ed9cb670c3658c695a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec1fe8743d230453743b",
+    "url": "/static/css/main.5427cca4.chunk.css"
+  },
+  {
+    "revision": "d0da0f63be153a967ccf",
+    "url": "/static/js/2.016c5ef6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.016c5ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1fe8743d230453743b",
+    "url": "/static/js/main.bf9d37c2.chunk.js"
+  },
+  {
+    "revision": "c20b7e9229712c76d7a0",
+    "url": "/static/js/runtime-main.339ab150.js"
+  },
+  {
+    "revision": "079dd0dc85f1142b95e78d9e5c71d6d3",
+    "url": "/static/media/logo.079dd0dc.png"
+  }
+]);
